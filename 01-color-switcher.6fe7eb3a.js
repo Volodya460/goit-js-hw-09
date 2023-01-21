c5bc7272ad1b5f5b0f5f57264364b291b3823ffb
Volodya460 <110915611+Volodya460@users.noreply.github.com>
2023-01-21 22:20:31 +0000

    Deploying to gh-pages from @ Volodya460/goit-js-hw-09@140d7a57182ab481267db3a9f1c58542b4f456e6 🚀

---
+++ 01-color-switcher.6fe7eb3a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.6fe7eb3a.js.map
